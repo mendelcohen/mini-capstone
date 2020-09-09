45be33f6a421657793511fd8132f9443763c4c22
Mendel Cohen <mmhakohen@gmail.com>
2020-09-09 14:20:24 -0400

    creates path to first product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductsController < ApplicationController
     @all_products = Product.all
     render "products.json.jb" 
   end
+
+  def one_product
+    @one_product = Product.first
+    render "one_product.json.jb" 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/all_products" => "products#all_products"
+    get "/a_product" => "products#one_product"
   end
 end
